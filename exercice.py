8663211cf912cc687cdccddca1089074eb30bfea
Rosie Morissette <rosie.morissette@polymtl.ca>
2021-02-02 13:19:10 -0500

    exch4_modifié

--- exercice.py
+++ exercice.py
@@ -3,24 +3,53 @@
 
 
 def is_even_len(string: str) -> bool:
-    pass
+
+    # autre solution
+    is_pair = len(string) % 2 == 0
+
+    return is_pair
+
+
+    # autre solution
+    """ if len(string) % 2 == 0:
+        return True
+
+    return false """
+
 
 
 def remove_third_char(string: str) -> str:
-    pass
+    return string[0:2] + string[3:]
+
+# Équivalent de ce qui est érit dans la définition de la fonction:
+# replace_char("bonjour" , "o", "z") -> bznjzur
+
+def replace_char(string: str, old_char: str, new_char: str) -> str: #sans utiliser la fonction replace
+   # for char in string:
+    for i in range(len(string)):
+        if string[i] == old_char:
+            string = string[:i] + new_char + string[i + 1:]
+
+    return string
 
 
-def replace_char(string: str, old_char: str, new_char: str) -> str:
-    pass
 
+def get_number_of_char(string: str, char: str) -> int: # sans utiliser .count
+    num_char = 0
+    for i in string:
+        if i == char:
+            num_char += 1
 
-def get_number_of_char(string: str, char: str) -> int:
-    pass
+    return num_char
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    pass
+    num_words = 0
+    sentence_splited = sentence.split(' ')
+    for i in sentence_splited:
+        num_words += 1
 
+    return num_words
 
 def main() -> None:
     chaine = "Bonjour!"
@@ -36,7 +65,7 @@ def main() -> None:
     print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
 
     print(f"Le nombre d'occurrence de l dans hello est : {get_number_of_char(chaine, 'l')}")
-    
+
     chaine = "Baby shark doo doo doo doo doo doo"
     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
 
